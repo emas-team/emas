15680283f49088cccbfed969264cecd732458645
Krzysztof Podsiadlo <krzysztof.podsiadlo@gmail.com>
2015-12-13 19:07:29 +0100

    Integrated with agents. Saving results to file.

--- src/emas/core/EmasApp.java
+++ src/emas/core/EmasApp.java
@@ -2,36 +2,42 @@ package emas.core;
 
 import emas.agents.Agent;
 import emas.agents.Genotype;
+import emas.agents.services.IService;
+import emas.core.utils.ResultWriter;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class EmasApp {
 
     private static final int AGENTS = 10;
     private static final int MAX_GENERATIONS = 1000;
     private List<Agent> agents;
-    private Random random;
-    private CrossoverService crossoverService;
-    private MeetingService meetingService;
-
-    public EmasApp() {
-        random = new Random();
-    }
+    ResultWriter resultWriter;
 
     public void start() {
         createAgents();
+        resultWriter = new ResultWriter();
         for (int generation = 0; generation < MAX_GENERATIONS; generation++) {
             performGenerationActions();
+        }
+        try {
+            resultWriter.generateDiagram();
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            Logger.getLogger(EmasApp.class.toString()).log(Level.SEVERE, "Could not write results to file.", e);
         }
     }
 
     private void createAgents() {
         agents = new LinkedList<>();
         for (int i = 0; i < AGENTS; i++) {
-            agents.add(new Agent(new Genotype())); //TODO: Probably the new Genotype should be created in Agent's constructor
+            agents.add(new Agent());
         }
     }
 
@@ -40,20 +46,22 @@ public class EmasApp {
         for (Iterator<Agent> iterator = agents.iterator(); iterator.hasNext(); ) {
             Agent agent1 = iterator.next();
             Agent agent2 = iterator.next();
-            if (random.nextBoolean()) {
-                newAgents.add(crossoverService.perform(agent1, agent2));
-            } else {
-                meetingService.perform(agent1, agent2);
-            }
+            Agent newAgent = agent1.getService().doAction();
             verifyIfAgentIsAlive(agent1);
             verifyIfAgentIsAlive(agent2);
+            if(verifyIfAgentIsAlive(newAgent)) {
+                newAgents.add(newAgent);
+            }
         }
         agents.addAll(newAgents);
+        resultWriter.saveGeneration(agents);
     }
 
-    private void verifyIfAgentIsAlive(Agent agent) {
-        if (agent.getFitness() == 0) {
+    private boolean verifyIfAgentIsAlive(Agent agent) {
+        if (agent.evaluate() == 0) {
             agents.remove(agent);
+            return false;
         }
+        return true;
     }
 }
