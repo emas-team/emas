842077e700d1d94754c3c39b851dfdd9660e9916
Krzysztof Podsiadło <krzysztof.podsiadlo@gmail.com>
2015-12-21 01:04:43 +0100

    Tests after implementing islands

--- src/test/java/emas/core/utils/ResultWriterTest.java
+++ src/test/java/emas/core/utils/ResultWriterTest.java
@@ -1,6 +1,5 @@
-/*package emas.core.utils;
+package emas.core.utils;
 
-import emas.agents.Agent;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,9 +11,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class ResultWriterTest {
     private ResultWriter resultWriter;
@@ -47,39 +43,5 @@ public class ResultWriterTest {
         }
         assertEquals(generations.get(0), actual.get(0));
         assertEquals(generations.get(1), actual.get(1));
-    }*/
-
- /*   @Test
-    public void testSaveGeneration() throws Exception {
-        List<Agent> agentList = new ArrayList<>();
-        Agent agent = mock(Agent.class);
-        double expected = 10.;
-        when(agent.getFitness()).thenReturn(expected);
-        agentList.add(agent);
-
-        resultWriter.saveGeneration(agentList);
-
-        Field f = resultWriter.getClass().getDeclaredField("generations");
-        f.setAccessible(true);
-        List generation = (List) f.get(resultWriter);
-
-        assertTrue(generation.size() > 0);
-        Double actual = (Double) generation.get(0);
-        assertEquals((Object)expected, actual);
     }
-
-    @Test
-    public void testGetBest() throws Exception {
-        List<Agent> agentList = new ArrayList<>();
-        double value1 = 10.;
-        double value2 = 5.76;
-        Agent agent1 = mock(Agent.class);
-        Agent agent2 = mock(Agent.class);
-        when(agent1.getFitness()).thenReturn(value1);
-        when(agent2.getFitness()).thenReturn(value2);
-        agentList.add(agent1);
-        agentList.add(agent2);
-
-        assertEquals(5.76, resultWriter.getBest(agentList), 0.01);
-    }
-}*/
+}
